e35eb4e1306cd42ec8a1ad45c1564beb47ed6c41
Radosław Kluge <126408554+klugeradoslaw@users.noreply.github.com>
2024-03-22 09:51:04 +0100

    Update TableComponent.tsx

--- 01-frontend/TableComponent.tsx
+++ 01-frontend/TableComponent.tsx
@@ -1,73 +1,60 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import {ReactComponent as Circle} from '../../assets/icons/circle.svg'
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import {IconButton, Stack, Typography} from '@mui/material';
-import {InvoiceStatus, InvoiceStatusType} from "../../const/InvoiceStatus";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { IconButton, Stack, Typography } from "@mui/material";
+import { InvoiceStatus, InvoiceStatusType } from "../../const/InvoiceStatus";
 
-type Props = {
-    columns: {
-        accessorKey: string;
-        header: string;
-    }
-    data: {
-        
-    }
-}
+type Props<T> = {
+  columns: Array<{ accessorKey: keyof T; header: string }>;
+  data: T[];
+};
 
-const TableComponent = ({columns, data}: Props) => {
-    const setCircleColor = (invoiceStatus: string) => {
-        return InvoiceStatus.find(
-            (status: InvoiceStatusType) => status.title === invoiceStatus)?.fillColor;
-    };
-
+function TableComponent<T>({ data, columns }: Props<T>) {
+  const setCircleColor = (invoiceStatus: string) => {
+    return InvoiceStatus.find(
+      (status: InvoiceStatusType) => status.title === invoiceStatus)?.fillColor;
+  };
+  
     return (
-        <TableContainer>
-            <Table sx={{minWidth: 650, fontFamily: 'Roboto',}} aria-label="invoice-table">
-                <TableHead>
-                    <TableRow>
-                        {columns.map(column => (
-                            <TableCell key={column.accessorKey}>
-                                {column.header}
-                            </TableCell>
-                        ))}
-                        <TableCell />
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((row) => (
-                        <TableRow key={row.id}>
-                            {columns.map(column => (
-                                <TableCell key={column.accessorKey}>
-                                    <Stack
-                                        direction="row"
-                                        spacing={1}
-                                        justifyContent="flex-start"
-                                        alignItems="center"
-                                    >
-                                        {column.accessorKey === 'invoiceStatus' &&
-                                            <Circle fill={setCircleColor(row.invoiceStatus)}/>}
-                                        <Typography>
-                                            {row[column.accessorKey]}
-                                        </Typography>
-                                    </Stack>
-                                </TableCell>
-                            ))}
+    <TableContainer>
+      <Table
+        sx={{ minWidth: 650, fontFamily: "Roboto" }}
+        aria-label="invoice-table"
+      >
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column.accessorKey.toString()}>
+                {column.header}
+              </TableCell>
+            ))}
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, rowId) => (
+            <TableRow key={rowId}>
+              {columns.map((column) => (
+                <TableCell key={column.accessorKey.toString()}>
+                  {row[column.accessorKey]}
+                </TableCell>
+              ))}
 
-                            <TableCell sx={{padding: 0}}>
-                                <IconButton onClick={null}><MoreVertIcon/></IconButton>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+              <TableCell sx={{ padding: 0 }}>
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
-
 export default TableComponent;
